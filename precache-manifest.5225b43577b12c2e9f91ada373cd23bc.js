c80bc7f965dc187c298c82b80a9aa405e9c2b648
nafeu-pelmorex <nnasir@pelmorex.com>
2020-02-20 11:04:31 -0500

    Updates

---
+++ precache-manifest.5225b43577b12c2e9f91ada373cd23bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97af7e87981beba5882ec24af16abddb",
+    "url": "/weather-stellar/index.html"
+  },
+  {
+    "revision": "0fd4b26dda8974218e89",
+    "url": "/weather-stellar/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6afd4bd0fb6bf03bd6c6",
+    "url": "/weather-stellar/static/js/2.5e2f6f9a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/weather-stellar/static/js/2.5e2f6f9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fd4b26dda8974218e89",
+    "url": "/weather-stellar/static/js/main.c39b95b0.chunk.js"
+  },
+  {
+    "revision": "a6e883b36cb946ab71c6",
+    "url": "/weather-stellar/static/js/runtime-main.aec5ec36.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/weather-stellar/static/media/logo.5d5d9eef.svg"
+  }
+]);
